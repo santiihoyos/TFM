7e600b1c770c8a4b2bb14ac4aced4cbf553f394d
Unknown <santiago.hoyos93@gmail.com>
2018-04-20 20:12:02 +0200

    Standar assets

---
+++ TFM/Assets/Scripts/prototipado/ControlZombie.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlZombie : MonoBehaviour
+{
+  
+  private Animator _animador;
+  private Rigidbody _rigidbody;
+  
+  // Use this for initialization
+  private void Start()
+  {
+    _animador = GetComponent<Animator>();
+    _rigidbody = GetComponent<Rigidbody>();
+  }
+
+  // Update is called once per frame
+  private void Update()
+  {
+    var izquierdaDerecha = Input.GetAxis("Horizontal");
+    var delanteAtras = Input.GetAxis("Vertical");
+    _rigidbody.AddForce(Vector3.forward * delanteAtras * 20f, ForceMode.Acceleration);
+  }
+}
