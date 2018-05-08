17c0a922258cf63ed5c24f51fc3f2f7ff082d5ee
Unknown <santiago.hoyos93@gmail.com>
2018-05-08 22:36:54 +0200

    implementado partícula de disparo

--- TFM/Assets/ControladorJugador.cs
+++ TFM/Assets/ControladorJugador.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditorInternal;
 using UnityEngine;
@@ -10,25 +11,43 @@ public class ControladorJugador : MonoBehaviour
   public Animator animadorArma;
   private CharacterController _controller;
 
+  [SerializeField] private GameObject[] prefabsImpactos;
+
   // Use this for initialization
   void Start()
   {
     _controller = GetComponent<CharacterController>();
   }
 
-  // Update is called once per frame
-  void Update()
-  {    
-
-  }
-
   private void FixedUpdate()
   {
     var isCorriendo = Mathf.Abs(_controller.velocity.x) > 5 || Mathf.Abs(_controller.velocity.z) > 5;
-    
+    RaycastHit hit;
+    Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+    Debug.DrawRay(ray.origin, ray.direction * 100, Color.green);
+
     if (!isCorriendo && Input.GetButtonDown("Fire1"))
     {
       animadorArma.SetTrigger("disparando");
+
+      if (Physics.Raycast(ray, out hit, 100)) //Disparo choca!!!
+      {
+        switch (hit.collider.sharedMaterial.name)
+        {
+          case "Metal":
+            Instantiate(prefabsImpactos[0], hit.point, hit.transform.rotation);
+            break;
+          case "Wood":
+            Instantiate(prefabsImpactos[1], hit.point, hit.transform.rotation);
+            break;
+          case "Meat":
+            Instantiate(prefabsImpactos[2], hit.point, hit.transform.rotation);
+            break;
+          case "Stone":
+            Instantiate(prefabsImpactos[3], hit.point, hit.transform.rotation);
+            break;
+        }
+      }
     }
 
     animadorArma.SetBool("corriendo", isCorriendo);
