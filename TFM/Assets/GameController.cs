a6df0d91a1ecb54cd2b06c2f0c64593382b239e3
Unknown <santiago.hoyos93@gmail.com>
2018-05-23 23:22:58 +0200

    animacion aterrizaje helicoptero

---
+++ TFM/Assets/GameController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+ 
+using System.Collections.Generic;
+ 
+using UnityEngine;
+ 
+using UnityEngine.Audio;
+ 
+
+ 
+public class GameController : MonoBehaviour
+ 
+{
+	public AudioClip[] SonidosDeFondo;
+ 
+
+ 
+	private AudioSource _audioSourceGeneral;
+ 
+	private int _sonidoDeFondoActual;
+ 
+	private AudioMixer _audioMixer;
+ 
+  
+ 
+	// Use this for initialization
+ 
+	void Start ()
+ 
+	{
+ 
+		_audioSourceGeneral = GetComponent<AudioSource>();
+ 
+	}
+ 
+  
+ 
+	// Update is called once per frame
+ 
+	void Update () {
+ 
+
+ 
+		if (!_audioSourceGeneral.isPlaying)
+ 
+		{
+ 
+			if (_sonidoDeFondoActual + 1 >= SonidosDeFondo.Length)
+ 
+			{
+ 
+				_sonidoDeFondoActual = 0;
+ 
+			}
+ 
+			else
+ 
+			{
+ 
+				_sonidoDeFondoActual += 1;
+ 
+			}
+ 
+			_audioSourceGeneral.clip = SonidosDeFondo[_sonidoDeFondoActual];
+ 
+			_audioSourceGeneral.Play();
+ 
+		}
+ 
+    
+ 
+	}
+ 
+}
